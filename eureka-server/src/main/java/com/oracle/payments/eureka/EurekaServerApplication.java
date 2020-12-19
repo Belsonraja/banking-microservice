11ea1918fd7cdea5bc9e4a460db3c4c7db77fbf1
BELSONRAJA T <32982789+Belsonraja@users.noreply.github.com>
2020-12-19 20:34:38 +0530

    Initial commit
    Initial commit

---
+++ eureka-server/src/main/java/com/oracle/payments/eureka/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.oracle.payments.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
