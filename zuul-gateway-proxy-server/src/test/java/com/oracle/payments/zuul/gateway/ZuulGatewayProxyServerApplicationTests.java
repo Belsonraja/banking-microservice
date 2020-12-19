bb652c9a48f1619c56b74f3e7cb0aaeae5448989
BELSONRAJA T <32982789+Belsonraja@users.noreply.github.com>
2020-12-19 20:35:36 +0530

    Initial commit
    Initial commit

---
+++ zuul-gateway-proxy-server/src/test/java/com/oracle/payments/zuul/gateway/ZuulGatewayProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.payments.zuul.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayProxyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
